e752ab1881aca2cac1377b6518c27454ba7b539c
sradms0 <sradms0@openmailbox.org>
2019-06-12 16:21:06 -0400

    add ch02 pr05

---
+++ 02/projects/05/5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float x = 0.0f;
+
+    printf("Enter a value for x: ");
+    if (scanf("%f", &x) > 0) {
+        printf("%.2f\n", 3.0f*(x*x) + 2.0f*(x*x*x*x) - 5.0f*(x*x*x) - x*x + 7.0f*x - 6);
+    } else {
+        printf("Invalid input.Please enter a number");
+    }
+
+    return 0;
+}
---
+++ 02/projects/05/README.md
@@ -0,0 +1,7 @@
+### Project 2.05
+Write a program that asks the user to enter a value for `x` and then displays the value of the following polynomial:
+`3x⁵ + 2x⁴ - 5x³ - x² + 7x - 6`
+
+### Solution
+See `5.c`
+
