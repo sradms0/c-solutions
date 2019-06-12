cfcb90aa7823c37b79aea96f44e7e48d47126daa
sradms0 <sradms0@openmailbox.org>
2019-06-12 16:46:01 -0400

    correct exponentiation in polynomial expression

--- 02/projects/05/5.c
+++ 02/projects/05/5.c
@@ -6,7 +6,7 @@ int main(void)
 
     printf("Enter a value for x: ");
     if (scanf("%f", &x) > 0) {
-        printf("%.2f\n", 3.0f*(x*x) + 2.0f*(x*x*x*x) - 5.0f*(x*x*x) - x*x + 7.0f*x - 6);
+        printf("%.2f\n", 3.0f*(x*x*x*x*x) + 2.0f*(x*x*x*x) - 5.0f*(x*x*x) - x*x + 7.0f*x - 6);
     } else {
         printf("Invalid input.Please enter a number");
     }
