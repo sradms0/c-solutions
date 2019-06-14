b5dafbba28aab7261acb61a167b74b70b1c624a9
sradms0 <sradms0@openmailbox.org>
2019-06-14 07:51:31 -0400

    add ch03 pr01

---
+++ 03/projects/01/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int month = 0, day = 0, year = 0;
+
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &month, &day, &year);
+
+    printf("You entered the date %d%d%d\n", year, month, day);
+
+    return 0;
+}
---
+++ 03/projects/01/README.md
@@ -0,0 +1,8 @@
+### Project 3.01
+Write a program that accepts a date from the user in the form *mm/dd/yy* and then displays it in the form *yyyymmdd:*
+```
+Enter a date (mm/dd/yyyy) 2/17/2011
+You entered the data 20110217
+```
+### Solution
+See `1.c`
