998d3c7315bf60e81972307c2c867594fb0a7f66
sradms0 <sradms0@openmailbox.org>
2019-08-12 10:26:46 -0400

    add ch09 pr01

---
+++ 09/projects/01/1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define N 8
+
+int main(void)
+{
+    int i = 0, ints[N];
+
+    printf("Enter %d integers: ", N);
+    while(i < N) scanf("%d", &ints[i++]);
+
+    selection_sort(N, ints);
+    printf("Sorted: ");
+    print_ints(N, ints);
+
+    return 0;
+}
+
+void selection_sort(int n, int ints[n]) 
+{
+    int i, temp, max = 0;
+
+    for (i = 1; i < n; i++) {
+        if (ints[i] > ints[max]) max = i;
+    }
+    temp = ints[n-1];
+    ints[n-1] = ints[max];
+    ints[max] = temp;
+
+    if (n > 1) selection_sort(n-1, ints);
+}
+
+void print_ints(int n, int ints[n]) {
+    int i;
+    for (i = 0; i < N; i++) printf("%d ", ints[i]);
+    printf("\n");
+}
