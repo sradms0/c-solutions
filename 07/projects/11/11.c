913bbba5dc145a9d3a8a2a4267fee12edcec41de
Scott Adams <sradms0@protonmail.com>
2021-08-17 09:20:44 -0400

    fix prompt-string and `.` placement to end of first name initial

--- 07/projects/11/11.c
+++ 07/projects/11/11.c
@@ -4,11 +4,11 @@ int main(void)
 {
     char first_initial, last_name;
 
-    printf("Enter a first_initial and last_name name: ");
+    printf("Enter a first and last name: ");
     while((first_initial = getchar()) == ' ');
     while(getchar() != ' ');
     while((last_name = getchar()) != '\n') putchar(last_name);
-    printf("., %c\n", first_initial);
+    printf(", %c.\n", first_initial);
 
     return 0;
 }
