1b863e5077a004447142ba10b7f424f1cee3912c
Scott Adams <sradms0@protonmail.com>
2021-08-17 09:10:34 -0400

    add ch13 pr09

---
+++ 13/projects/09/9.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define LENGTH 100
+
+int compute_vowel_count(const char *sentence);
+
+int main(void)
+{
+    char sentence[LENGTH+1];
+    char ch, *p = sentence;
+
+    printf("Enter a sentence: ");
+    while((ch = getchar()) != '\n')
+        *p++ = ch;
+    *p = '\0';
+
+    printf("Your sentence contains %d vowels.\n", compute_vowel_count(sentence));
+
+    return 0;
+}
+
+int compute_vowel_count(const char *sentence)
+{
+    int vowels = 0;
+
+    while(tolower(*sentence)) {
+        switch(*sentence++) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+                vowels++;
+            default:
+                break;
+        }
+    }
+
+    return vowels;
+}
